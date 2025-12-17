648415e5c36938217f016a50179b35739cef7e21
Jamil Lambert, PhD <Jamil.Lambert@proton.me>
2025-12-17 21:19:32 +0000

    Fix bitreq doc example
    The example in the docs cause a random CI failure.
    
    Change them so that they don't rely on example.com anymore.

--- bitreq/src/lib.rs
+++ bitreq/src/lib.rs
@@ -72,14 +72,27 @@
 //!
 //! ```
 //! # #[cfg(feature = "std")]
-//! # fn main() -> Result<(), Box<dyn std::error::Error>> {
-//! let response = bitreq::get("http://example.com").send()?;
+//! # fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+//! # use std::thread;
+//! # use tiny_http::{Response, Server};
+//! #
+//! # let server = Server::http("127.0.0.1:0")?;
+//! # let addr = server.server_addr().to_ip().expect("IP listen addr");
+//! # let server_thread = thread::spawn(move || {
+//! #     let request = server.recv().expect("server recv");
+//! #     let response = Response::from_string("<html></html>");
+//! #     let _ = request.respond(response);
+//! # });
+//! #
+//! # let url = format!("http://{addr}/");
+//! let response = bitreq::get(&url).send()?;
 //! assert!(response.as_str()?.contains("</html>"));
 //! assert_eq!(200, response.status_code);
 //! assert_eq!("OK", response.reason_phrase);
+//! # server_thread.join().expect("server thread join");
 //! # Ok(()) }
 //! # #[cfg(not(feature = "std"))]
-//! # fn main() -> Result<(), Box<dyn std::error::Error>> { Ok(()) }
+//! # fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> { Ok(()) }
 //! ```
 //!
 //! Note: you could change the `get` function to `head` or `put` or
